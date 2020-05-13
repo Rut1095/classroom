0cbcc57119f8251b24ebc467f5461c2dede1c2c5
Rut1095 <rutlandau@gmail.com>
2020-05-13 23:45:28 +0300

    first upload client

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShowCameraComponent } from './show-camera/show-camera.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShowCameraComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
