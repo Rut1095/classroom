f815108134ed83aef754f932c45ea68a2b9edbd1
Rut1095 <rutlandau@gmail.com>
2020-09-10 10:53:35 +0300

    commit

--- ClassRoomService__/ClassRoomService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ClassRoomService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
